0940e07ddf3083582e83c36a3741d331cb03d00a
RTerBorch <robin.ter.borch@gmail.com>
2023-12-10 19:42:49 +0100

    setup docker enviroment, and also enviroment var

--- src/main/java/com/CalorieCounter/CalorieCounter/controller/AdminController.java
+++ src/main/java/com/CalorieCounter/CalorieCounter/controller/AdminController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
     @Autowired
     LivsmedelRepository livsmedelRepository;
 
@@ -25,7 +26,6 @@ public class AdminController {
 
     @Autowired
     private LivsmedelService livsmedelService;
-
 
     @PostMapping("/addAccount")
     public String addUser(){
